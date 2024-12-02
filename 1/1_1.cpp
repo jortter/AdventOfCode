06879e5c61adb998581712f5274fc404fbf38199
jortter <jortter@etsinf.upv.es>
2024-12-02 14:52:53 +0100

    Vector ordenado

--- 1/1_1.cpp
+++ 1/1_1.cpp
@@ -15,11 +15,12 @@ void distVect(){
     	throw std::runtime_error("No se puede abrir el fichero");
 	}
 
-    // En este bucle 
+    // En este bucle pondremos el algoritmo del problema
     while(getline(archivo, linea)){
         // Extraer los valores numéricos de un string (lineas del fichero) uno por uno
         std::istringstream ss(linea);
         int numero1, numero2;
+
         ss >> numero1;
         n1.push_back(numero1);
         
@@ -27,10 +28,35 @@ void distVect(){
         n2.push_back(numero2);
     }
 
+    // Mostrar los vectores
     for(int i = 0; i < n1.size() && i < n2.size(); i++){
         std::cout << n1[i];
         std::cout << "    ";
         std::cout << n2[i] << std::endl;
+    }
+    std::cout << std::endl;
+    // Ordenar el vector n1 y n2
+    for(int i = 0; (i < n1.size()) && (i < n2.size()); i++){
+        int posmin1 = i;
+        int posmin2 = i;
+        for(int j = i+1; (j < n1.size()) && (j < n2.size()); j++){
+            if(n1[j] < n1[posmin1]){
+                posmin1 = j;
+            }
+            if(n2[j] < n2[posmin2]){
+                posmin2 = j;
+            }
+        }
+        if(posmin1 != i){
+            std::swap(n1[i], n1[posmin1]);
+        }
+        if(posmin2 != i){
+            std::swap(n2[i], n2[posmin2]);
+        }
+        std::cout << n1[i] << "    " << n2[i] << std::endl;
+
+        
+
     }
 
     archivo.close();
